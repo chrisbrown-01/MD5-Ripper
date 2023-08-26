bcf1cff514160e18912c75a75a1a0d3f4a824565
chrisbrown-01 <chrisbrown1@hotmail.ca>
2023-08-25 20:09:12 -0400

    create shared class library and benchmarking projects

---
+++ Benchmarking/Benchmarks.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Threading.Tasks;
+
+namespace Benchmarking
+{
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+            var a = 1;
+            var b = 2;
+            var c = a + b;
+        }
+
+        [Benchmark]
+        public async Task Scenario2()
+        {
+            await Task.Delay(300);
+            var a = 1;
+            var b = 2;
+            var c = a + b;
+        }
+    }
+}
